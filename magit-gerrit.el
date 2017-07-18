703e52ef379faa7078b480fe28c9021b49845aea
Robin Lin <robin.l.lin@ericsson.com>
2017-07-18 14:47:16 +0200

    Keep the topic simple and same as before

--- magit-gerrit.el
+++ magit-gerrit.el
@@ -482,8 +482,7 @@ Succeed even if branch already exist
 	   (branch-pub (progn
 			 (string-match (rx "refs/heads" (group (one-or-more any)))
 				       branch-merge)
-			 (format "refs/%s%s/%s" status (match-string 1 branch-merge) branch))))
-
+			 (format "refs/%s%s/%s" status (match-string 1 branch-merge) (car (last (split-string branch "/" t split-string-default-separators)))))))
 
       (when (or (null branch-remote)
 		(string= branch-remote "."))
